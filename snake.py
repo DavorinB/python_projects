92542b827a24fd88aca53d823c09c7a9ba8170c0
dbosanac <davorin.bosanac@barrage.net>
2021-07-15 18:13:54 +0200

    too much tabs

--- snake.py
+++ snake.py
@@ -9,18 +9,45 @@ game_over=False
 blue=(0,0,255)
 red=(255,0,0)
 
+#Move var
+a1 = 300
+b1 = 300
+
+a1_change = 0
+b1_change = 0
+
 #This loop is needed to keep the screen running
 #while not game_over:
 #	for event in pygame.event.get():
 #		print(event) #show action on the screen
 
+clock = pygame.time.Clock()
 #Loop where you can actually quit the game
 while not game_over:
 	for event in pygame.event.get():
-		if event.type==pygame.QUIT:
-			game_over=True
+		if event.type == pygame.QUIT:
+			game_over = True
+		if event.TYPE == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+			a1_change = -10
+			b1_change = 0
+		elif event.key == pygame.K_RIGHT:
+			a1_change = 10
+			b1_change = 0
+		elif event.key == pygame.K_UP:
+			a1_change = -10
+			b1_change = 0
+		elif event.key == pygame.K_DOWN:
+			a1_change = 10
+			b1_change = 0
+	a1 += a1_change
+	b1 += b1_change
+	pygame.draw.rect(dis, black, [a1, b1, 10, 10])
+	pygame.display.update()
+	clock.tick(30)
+
 #Rectangle draw
 	pygame.draw.rect(dis,blue,[200,150,10,10])
-	pygame.display.update()			
+	pygame.display.update()
 pygame.quit ()
 quit ()
