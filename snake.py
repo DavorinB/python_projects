c4355d81eb061e35403b4676d72b1deebbcc7c09
dbosanac <davorin.bosanac@barrage.net>
2021-07-15 17:53:17 +0200

    colors&rect

--- snake.py
+++ snake.py
@@ -2,11 +2,25 @@ import pygame
 pygame.init ()
 dis=pygame.display.set_mode ((400,300))
 pygame.display.update ()
-pygame.set.caption('Test pygame')
-game_over=false
+pygame.display.set_caption('Test pygame')
+game_over=False
+#Snake color
+
+blue=(0,0,255)
+red=(255,0,0)
+
 #This loop is needed to keep the screen running
+#while not game_over:
+#	for event in pygame.event.get():
+#		print(event) #show action on the screen
+
+#Loop where you can actually quit the game
 while not game_over:
 	for event in pygame.event.get():
-		print(event) #show action on the screen
+		if event.type==pygame.QUIT:
+			game_over=True
+#Rectangle draw
+	pygame.draw.rect(dis,blue,[200,150,10,10])
+	pygame.display.update()			
 pygame.quit ()
 quit ()
