9c4e15c67fa201638463cc228db77cd2dd4312d5
dbosanac <davorin.bosanac@barrage.net>
2021-07-15 17:41:46 +0200

    display loop added

--- snake.py
+++ snake.py
@@ -2,5 +2,11 @@ import pygame
 pygame.init ()
 dis=pygame.display.set_mode ((400,300))
 pygame.display.update ()
+pygame.set.caption('Test pygame')
+game_over=false
+#This loop is needed to keep the screen running
+while not game_over:
+	for event in pygame.event.get():
+		print(event) #show action on the screen
 pygame.quit ()
 quit ()
